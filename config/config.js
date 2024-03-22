4d470e6396f157f0e9a221092d89f370a1c46400
Pankaj Khatri <4699pankajkhatri@gmail.com>
2024-03-22 22:34:23 +0530

    env work

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
-// const userEmail= (res)=>{res};
 module.exports = {
-  mongoUrl: "mongodb+srv://4699pankajkhatri:MongoDB4699@cluster0.y9qkfu5.mongodb.net/TaskManager",
-  jwtSecret: 'HelloMrPK',
+  mongoUrl: process.env.Mongo_url,
+  jwtSecret: process.env.Secret_key,
 };
