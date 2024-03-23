c67ab10bb5440f97527b8a72b815999d1a7525b7
Pankaj Khatri <4699pankajkhatri@gmail.com>
2024-03-23 21:07:54 +0530

    profile1

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,17 +7,17 @@ const { Secret_key } = require("../config/config");
 
 exports.register = async (req, res) => {
     try {
-        const {username, email, password}= req.body;
-        const existinguser= await User.findOne({email});
-        if(existinguser){
-         return res.status(404).json({error:'Existing User try to login'});
+        const { username, email, password } = req.body;
+        const existinguser = await User.findOne({ email });
+        if (existinguser) {
+            return res.status(404).json({ error: 'Existing User try to login' });
         }
-        const hashedPassword= await bcrypt.hash(password,4);
-        const user= new User({username, email, password:hashedPassword});
+        const hashedPassword = await bcrypt.hash(password, 4);
+        const user = new User({ username, email, password: hashedPassword });
         await user.save();
-        res.status(201).json({msg: 'User registered successfully'});
+        res.status(201).json({ msg: 'User registered successfully' });
     } catch (error) {
-        res.status(404).json({error:'Internal Server Error', msg:error})
+        res.status(404).json({ error: 'Internal Server Error', msg: error })
     }
 };
 
@@ -25,7 +25,7 @@ exports.login = async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await User.findOne({ email });
-        
+
         if (!user) {
             return res.status(401).json({ error: 'Invalid email' });
         }
@@ -36,8 +36,29 @@ exports.login = async (req, res) => {
         }
 
         const token = jwt.sign({ userID: user._id }, Secret_key, { expiresIn: '1h' });
-        res.status(201).json({ token , email, name:user.username});
+        res.status(201).json({ token, email, name: user.username });
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+exports.getUserProfile = async (req, res) => {
+    try {
+        const userId = req.user.userID;
+        const user = await User.findById(userId).select("-password"); // Exclude password field
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+exports.updateUserProfile = async (req, res) => {
+    try {
+        const userId = req.user.userID;
+        const { username, email, age, about } = req.body;
+        await User.findByIdAndUpdate(userId, { username, email, age, about },{new:true});
+        res.json({ message: "User profile updated successfully" });
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
     }
 };
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const taskController = require("../controllers/taskController");
 const { authenticateToken } = require("../middleware/authmiddleware");
 
-router.get("/", authenticateToken, taskController.getAllTasks);
-router.post("/", authenticateToken, taskController.createTask);
-router.put("/:id", authenticateToken, taskController.updateTask);
-router.delete("/:id", authenticateToken, taskController.deleteTask);
+router.use(authenticateToken);
+router.get("/", taskController.getAllTasks);
+router.post("/", taskController.createTask);
+router.put("/:id",  taskController.updateTask);
+router.delete("/:id",  taskController.deleteTask);
 
 module.exports = router;
